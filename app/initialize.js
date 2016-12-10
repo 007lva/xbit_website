729a4223542845748e33f7ff02b24a111aad747a
luigi <007.lva@gmail.com>
2016-12-10 23:50:47 +0100

    Fix code style

--- app/initialize.js
+++ app/initialize.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var menu_list = document.querySelector('.xbt-NavigationList');
 
   document.querySelector('[data-menu]').addEventListener('click', function(e) {
-              e.preventDefault();
-              menu_list.classList.toggle('active');
+    e.preventDefault();
+    menu_list.classList.toggle('active');
   });
-})
+});
